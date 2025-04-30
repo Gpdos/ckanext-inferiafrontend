ddaa8c3c126b0cce31fe66191df3660e0a280a7b
Gpdos <samuel.rebollofructuoso@gmail.com>
2025-04-30 10:52:22 +0200

    Arreglo

--- ckanext/inferiafrontend/plugin.py
+++ ckanext/inferiafrontend/plugin.py
@@ -1,5 +1,6 @@
-from ckan.plugins import implements, IConfigurer, SingletonPlugin
+import os
 from ckan import config
+from ckan.plugins import implements, IConfigurer, SingletonPlugin
 
 class InferiaFrontendPlugin(SingletonPlugin):
     implements(IConfigurer)
@@ -8,14 +9,13 @@ class InferiaFrontendPlugin(SingletonPlugin):
         """
         Configura los archivos estáticos y las plantillas del frontend.
         """
-        # Añadir el directorio de plantillas de tu plugin
-        template_path = '/path/to/your/plugin/templates'
-        static_path = '/path/to/your/plugin/public'
+        # Ruta de la plantilla y archivos estáticos usando la ubicación del plugin
+        plugin_dir = os.path.dirname(os.path.abspath(__file__))
+        template_path = os.path.join(plugin_dir, 'templates')
+        static_path = os.path.join(plugin_dir, 'public')
 
         # Registra el directorio de plantillas
         config['ckan.plugins.template_dirs'] = template_path
 
         # Registra los recursos estáticos
         config['ckan.plugins.static_dirs'] = static_path
-
-        # Añadir más configuraciones si es necesario
